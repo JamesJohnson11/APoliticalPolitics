5248007ce5f66191237493c93c8de692b038673a
Barry Johnson <bdotmgmt@gmail.com>
2020-09-05 22:47:41 -0400

    Added template, form, view and url for Search functionality.

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post, Comment
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
-from .forms import CommentForm
+from django.contrib.postgres.search import SearchVector
+from .forms import CommentForm, SearchForm
 
 
 def post_share(request, post_id):
@@ -37,5 +38,19 @@ def post_detail(request, year, month, day, post):
     else:
         comment_form = CommentForm()
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments,'new_comment': new_comment, 'comment_form': comment_form})
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            results = Post.published.annotate(
+                search=SearchVector('title', 'body'),
+            ).filter(search=query)
+    return render(request, 'blog/post/search.html', {'form': form, 'query': query, 'results': results})
 
 
